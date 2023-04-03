9571f779b4f3e5c26d1bfab596504d67e6d913bc
ndnastia <ndnastiadatsiuk@gmail.com>
2023-04-03 19:07:56 +0200

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', onClick);
+function onClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+       return;
+    }
+
+    const imgSource = evt.target.dataset.source;
+    const instance = basicLightbox.create(
+      `<img src="${imgSource}"/>`,
+    );
+
+    instance.show();
+
+
+   gallery.addEventListener("keydown", onKeydown);
+
+    function onKeydown(evt) {
+          if (evt.code === "Escape") {
+              instance.close();
+           gallery.removeEventListener("keydown", onKeydown);   
+          }
+    };
+       
+    }
+
+   
+    
+     
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a");
+}
